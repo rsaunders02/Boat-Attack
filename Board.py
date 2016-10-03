fc4bfd78662d60e0028e58be4dad361608b1ba3b
Raina Saunders <resaunders02@gmail.com>
2016-10-03 13:06:15 -0700

    ah 2

--- Board.py
+++ Board.py
@@ -1,28 +1,12 @@
 class Board():
     def __init__(self):
+        self.board = []
         self.letters_to_columns = {"a":1, "b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10}
         #set up board
-        self.board = [["0" for 11 in range(11)] for 11 in range(11)]
-        self.board[0][1] = "1"
-        self.board[0][2] = "2"
-        self.board[0][3] = "3"
-        self.board[0][4] = "4"
-        self.board[0][5] = "5"
-        self.board[0][6] = "6"
-        self.board[0][7] = "7"
-        self.board[0][8] = "8"
-        self.board[0][9] = "9"
-        self.board[0][10] = "10"
-        self.board[1][0] = "A"
-        self.board[2][0] = "B"
-        self.board[3][0] = "C"
-        self.board[4][0] = "D"
-        self.board[5][0] = "E"
-        self.board[6][0] = "F"
-        self.board[7][0] = "G"
-        self.board[8][0] = "H"
-        self.board[9][0] = "I"
-        self.board[10][0] = "J"
+        for row in range(11):
+            for col in range(11):
+                self.board.append("0")
+        print self.board
 
     """ This method passes the coordinates of a ship to the
         method and places the pieces on the board.
